6f6cc44485112d6e0c19fedf9ffb5d26d67f7e7b
Raschlemper <raschlemper@gmail.com>
2016-11-21 21:37:32 -0200

    ajustes bower

--- server/config/environment/development.js
+++ server/config/environment/development.js
@@ -3,6 +3,11 @@
 // Development Specific Configuration
 // ==================================
 module.exports = {
+  // Server IP
+  ip:       '127.0.0.1',
+
+  // Server port
+  port:     3000,
   mongo: {
     uri: 'mongodb://localhost/protuario'
   }
